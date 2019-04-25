4ae220a03e7f8ba928c3a03d063cee90f37f1f6d
Francesca Romana Gambini <francesca.rgambini@gmail.com>
2019-04-25 10:03:10 +0100

    fixed some bugs

--- hellosunapp.js
+++ hellosunapp.js
@@ -1,11 +1,15 @@
-let h2 = document.querySelector("h2");
+let place = document.querySelector("h2");
 let getTemperature = document.querySelector(".getTemperature");
+console.log(getTemperature);
 let sunriseHour = document.querySelector(".sunriseHour");
 let sunsetHour = document.querySelector(".sunsetHour");
 let currentHumidity = document.querySelector(".currentHumidity");
 let currentWindSpeed = document.querySelector(".currentWindSpeed");
+let currentDate = document.querySelector(".currentDate");
+let currentHour = document.querySelector(".currentHour");
 let date = new Date();
-let city = "Lisbon";
+let input = document.querySelector("#location");
+let description = document.querySelector("#weather-description");
 
 let apiKey = "0d863e0f3f7819cc1dc71c86924341c5";
 let apiRoot = "https://api.openweathermap.org/data/2.5";
@@ -40,37 +44,30 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let currentDate = document.querySelector(".currentDate");
-  let currentHour = document.querySelector(".currentHour");
-  currentDate.innerHTML = `${day}, ${month} ${date.getDate()}, ${date.getFullYear()}`;
-  currentHour.innerHTML = `${date.getHours()}h${minutes}`;
+  return `${day}, ${month} ${date.getDate()}, ${date.getFullYear()}`;
 }
 
 function refreshWeather(response) {
-  event.preventDefault();
-  let apiPath = `/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiRoot}/${apiPath}`).then(function(response) {
-    h2.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
-    getTemperature.innerHTML = `${Math.round(response.data.main.temp)}º`;
-    sunriseHour.innerHTML = `Sunrise: ${response.data.sys.sunrise}`;
-    sunsetHour.innerHTML = `Sunset: ${response.data.sys.sunset}`;
-    currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-    currentWindSpeed.innerHTML = `Wind: ${response.data.wind.speed} km/H`;
-  });
+  console.log(response.data);
+  place.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  getTemperature.innerHTML = `${Math.round(response.data.main.temp)}º`;
+  sunriseHour.innerHTML = `Sunrise: ${response.data.sys.sunrise}`;
+  sunsetHour.innerHTML = `Sunset: ${response.data.sys.sunset}`;
+  currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  currentWindSpeed.innerHTML = `Wind: ${response.data.wind.speed} km/H`;
+  currentDate.innerHTML = `${day}, ${month} ${date.getDate()}, ${date.getFullYear()}`;
+  currentHour.innerHTML = `${date.getHours()}h${minutes}`;
+  description.innerHTML = `${response.data.weather[1]}`;
 }
+let city = "London";
+let apiUrl = `${apiRoot}/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
 
 function search(event) {
   event.preventDefault();
   let inputValue = document.querySelector("#input-value");
-  let apiPath = `/weather?q=${inputValue}&appid=${apiKey}&units=metric`;
+  let apiPath = `weather?q=${inputValue}&appid=${apiKey}&units=metric`;
   axios.get(`${apiRoot}/${apiPath}`).then(refreshWeather);
   h2.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
 }
-let input = document.querySelector("form");
 input.addEventListener("submit", search);
-
-navigator.geolocation.getCurrentPosition(function(position) {
-  let apiPath = `/weather?lat=${position.coords.latitude}&lon=${
-    position.coords.longitude
-  }&appid=${apiKey}&units=metric`;
-});
