f94bea7f4a194ec801c5c007d429682a9cd73f07
Francesca Romana Gambini <francesca.rgambini@gmail.com>
2019-04-26 12:27:24 +0200

    added weather description

--- hellosunapp.js
+++ hellosunapp.js
@@ -1,15 +1,15 @@
 let place = document.querySelector("h2");
 let getTemperature = document.querySelector(".getTemperature");
-console.log(getTemperature);
 let sunriseHour = document.querySelector(".sunriseHour");
 let sunsetHour = document.querySelector(".sunsetHour");
 let currentHumidity = document.querySelector(".currentHumidity");
 let currentWindSpeed = document.querySelector(".currentWindSpeed");
+let description = document.querySelector(".weatherDescription");
+console.log(description);
 let currentDate = document.querySelector(".currentDate");
 let currentHour = document.querySelector(".currentHour");
 let date = new Date();
 let input = document.querySelector("#location");
-let description = document.querySelector("#weather-description");
 
 let apiKey = "0d863e0f3f7819cc1dc71c86924341c5";
 let apiRoot = "https://api.openweathermap.org/data/2.5";
@@ -48,16 +48,15 @@ function formatDate(date) {
 }
 
 function refreshWeather(response) {
-  console.log(response.data);
   place.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
   getTemperature.innerHTML = `${Math.round(response.data.main.temp)}º`;
   sunriseHour.innerHTML = `Sunrise: ${response.data.sys.sunrise}`;
   sunsetHour.innerHTML = `Sunset: ${response.data.sys.sunset}`;
   currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   currentWindSpeed.innerHTML = `Wind: ${response.data.wind.speed} km/H`;
+  description.innerHTML = `${response.data.weather[0].description}`;
   currentDate.innerHTML = `${day}, ${month} ${date.getDate()}, ${date.getFullYear()}`;
   currentHour.innerHTML = `${date.getHours()}h${minutes}`;
-  description.innerHTML = `${response.data.weather[1]}`;
 }
 let city = "London";
 let apiUrl = `${apiRoot}/weather?q=${city}&appid=${apiKey}&units=metric`;
